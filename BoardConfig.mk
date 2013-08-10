7eacd24f88f973dc290fdee3275cd67a648794ae
Dorian Snyder <dastin1015@gmail.com>
2013-08-10 14:50:03 -0700

    shooter: update for shooter-common

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -24,8 +24,8 @@
 # variant, so that it gets overwritten by the parent (which goes
 # against the traditional rules of inheritance).
 
-# inherit from common msm8660
--include device/htc/msm8660-common/BoardConfigCommon.mk
+# inherit from common shooter
+-include device/htc/shooter-common/BoardConfigCommon.mk
 
 # inherit from the proprietary version
 -include vendor/htc/shooter/BoardConfigVendor.mk
@@ -36,11 +36,6 @@ TARGET_BOOTLOADER_BOARD_NAME := shooter
 
 BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := shooter
 
-BOARD_USE_NEW_LIBRIL_HTC := true
-
-# 3D Support
-TARGET_HARDWARE_3D := true
-
 # CDMA Ril and Wimax Support
 BOARD_USES_LEGACY_RIL := true
 COMMON_GLOBAL_CFLAGS += -DBOARD_HAVE_SQN_WIMAX
@@ -48,21 +43,10 @@ BOARD_HAVE_SQN_WIMAX := true
 
 # Kernel Details
 TARGET_KERNEL_CONFIG := shooter_defconfig
-
-# Camera
-TARGET_DISABLE_ARM_PIE := true
-BOARD_CAMERA_USE_MM_HEAP := true
-BOARD_HTC_3D_SUPPORT := true
-
-# Bluetooth/Wifi
--include device/htc/msm8660-common/bcmdhd.mk
-
-# Custom LUN File Path
-TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/class/android_usb/android0/f_mass_storage/lun0/file
-
 BOARD_KERNEL_CMDLINE := console=ttyHSL0 androidboot.hardware=shooter no_console_suspend=1
-BOARD_KERNEL_BASE := 0x48000000
-BOARD_KERNEL_PAGE_SIZE := 2048
+
+# Releasetools
+TARGET_RELEASETOOLS_EXTENSIONS := device/htc/shooter
 
 # cat /proc/emmc
 #dev:        size     erasesize name
@@ -82,23 +66,12 @@ BOARD_KERNEL_PAGE_SIZE := 2048
 # mmcblk0p8:  00c00200 00000200 "wimax"
 # mmcblk0p33: 007ffa00 00000200 "udata_wimax"
 
-TARGET_USERIMAGES_USE_EXT4 := true
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 435941376
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 1394606080
 BOARD_BOOTIMAGE_PARTITION_SIZE := 5242880
-BOARD_FLASH_BLOCK_SIZE := 262144
 
 RECOVERY_FSTAB_VERSION := 2
 TARGET_RECOVERY_FSTAB := device/htc/shooter/ramdisk/fstab.shooter
 TARGET_RECOVERY_PIXEL_FORMAT := "BGRA_8888"
 TARGET_RECOVERY_UI_LIB := librecovery_ui_shooter
 TARGET_RECOVERY_INITRC := device/htc/shooter/recovery/init.rc
-BOARD_SDCARD_DEVICE_PRIMARY := /dev/block/mmcblk1p1
-BOARD_SDCARD_DEVICE_SECONDARY := /dev/block/mmcblk1
-BOARD_SDEXT_DEVICE := /dev/block/mmcblk1p2
-BOARD_USES_MMCUTILS := true
-BOARD_HAS_NO_MISC_PARTITION := true
-BOARD_HAS_NO_SELECT_BUTTON := true
-
-# Releasetools
-TARGET_RELEASETOOLS_EXTENSIONS := device/htc/shooter
