a488287b2d543101383c1bd06904a02db118dc43
Dorian Snyder <dastin1015@gmail.com>
2014-05-04 16:17:26 -0700

    shooter: on new kernels lunfiles are somewhere else

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -83,6 +83,7 @@ BOARD_HAS_NO_REAL_SDCARD := false
 TW_NO_SCREEN_BLANK := true
 HAVE_SELINUX := true
 TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/platform/msm_hsusb/gadget/lun0/file"
 SP1_NAME := "wimax"
 SP1_DISPLAY_NAME := "WiMAX"
 SP1_BACKUP_METHOD := image
